240f2b58eec242db6aebeed476e009a8676dde4b
James <yrondon@gmail.com>
2024-06-07 18:09:37 -0500

    XD

--- Cliente/Formprincipal.Designer.cs
+++ Cliente/Formprincipal.Designer.cs
@@ -136,7 +136,7 @@ namespace Cliente
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "DESKTOP-NM28KG4\\MSSQLSERVER01",
-            "DESKTOP-MSSFJKR\\\\SQLEXPRESS"});
+            "DESKTOP-MSSFJKR\\SQLEXPRESS"});
             this.comboBox1.Location = new System.Drawing.Point(26, 77);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(230, 33);
