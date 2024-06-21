5bf45671698010f1d5e3cec8f846245db205a5e4
James <yrondon@gmail.com>
2024-06-21 18:17:36 -0500

    MANTENEDOR TIPO DE HABITACIONES COMPLETADO!

--- Cliente/manHabitaciones.Designer.cs
+++ Cliente/manHabitaciones.Designer.cs
@@ -290,7 +290,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(950, 478);
+            this.ClientSize = new System.Drawing.Size(950, 449);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
