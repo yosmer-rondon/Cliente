d731564afa525f546f70a2d97875043ddaa73dee
rafaelrm2001 <jhonatandriguesmedina@gmail.com>
2024-06-21 23:57:10 -0500

    aver si sale

--- Cliente/Formprincipal.Designer.cs
+++ Cliente/Formprincipal.Designer.cs
@@ -29,6 +29,7 @@ namespace Cliente
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Formprincipal));
             this.panel1 = new System.Windows.Forms.Panel();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.button7 = new System.Windows.Forms.Button();
@@ -56,16 +57,18 @@ namespace Cliente
             // 
             // panel1
             // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackgroundImage = global::Cliente.Properties.Resources.logo_hotel;
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Location = new System.Drawing.Point(587, 39);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(654, 506);
+            this.panel1.Size = new System.Drawing.Size(315, 102);
             this.panel1.TabIndex = 1;
             // 
             // tabPage3
             // 
-            this.tabPage3.BackgroundImage = global::Cliente.Properties.Resources.Captura;
+            this.tabPage3.BackgroundImage = global::Cliente.Properties.Resources.marco;
             this.tabPage3.Controls.Add(this.button7);
             this.tabPage3.Controls.Add(this.button6);
             this.tabPage3.Controls.Add(this.button5);
@@ -73,7 +76,7 @@ namespace Cliente
             this.tabPage3.Location = new System.Drawing.Point(4, 34);
             this.tabPage3.Margin = new System.Windows.Forms.Padding(5);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(952, 160);
+            this.tabPage3.Size = new System.Drawing.Size(952, 155);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Transacciones";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -83,9 +86,9 @@ namespace Cliente
             this.button7.BackColor = System.Drawing.Color.DimGray;
             this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button7.ForeColor = System.Drawing.Color.White;
-            this.button7.Location = new System.Drawing.Point(197, 20);
+            this.button7.Location = new System.Drawing.Point(39, 45);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(139, 34);
+            this.button7.Size = new System.Drawing.Size(195, 50);
             this.button7.TabIndex = 5;
             this.button7.Text = "Hospedaje";
             this.button7.UseVisualStyleBackColor = false;
@@ -96,9 +99,9 @@ namespace Cliente
             this.button6.BackColor = System.Drawing.Color.DimGray;
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button6.ForeColor = System.Drawing.Color.White;
-            this.button6.Location = new System.Drawing.Point(35, 79);
+            this.button6.Location = new System.Drawing.Point(527, 45);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(139, 34);
+            this.button6.Size = new System.Drawing.Size(205, 50);
             this.button6.TabIndex = 4;
             this.button6.Text = "Pago";
             this.button6.UseVisualStyleBackColor = false;
@@ -109,9 +112,9 @@ namespace Cliente
             this.button5.BackColor = System.Drawing.Color.DimGray;
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button5.ForeColor = System.Drawing.Color.White;
-            this.button5.Location = new System.Drawing.Point(35, 20);
+            this.button5.Location = new System.Drawing.Point(287, 45);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(139, 34);
+            this.button5.Size = new System.Drawing.Size(195, 50);
             this.button5.TabIndex = 3;
             this.button5.Text = "Reserva";
             this.button5.UseVisualStyleBackColor = false;
@@ -119,7 +122,7 @@ namespace Cliente
             // 
             // tabPage2
             // 
-            this.tabPage2.BackgroundImage = global::Cliente.Properties.Resources.Captura;
+            this.tabPage2.BackgroundImage = global::Cliente.Properties.Resources.marco;
             this.tabPage2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.tabPage2.Controls.Add(this.button9);
             this.tabPage2.Controls.Add(this.button8);
@@ -132,7 +135,7 @@ namespace Cliente
             this.tabPage2.Margin = new System.Windows.Forms.Padding(5);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(952, 160);
+            this.tabPage2.Size = new System.Drawing.Size(952, 155);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Mantenedores";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -142,9 +145,9 @@ namespace Cliente
             this.button9.BackColor = System.Drawing.Color.DimGray;
             this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button9.ForeColor = System.Drawing.Color.White;
-            this.button9.Location = new System.Drawing.Point(403, 41);
+            this.button9.Location = new System.Drawing.Point(690, 28);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(141, 62);
+            this.button9.Size = new System.Drawing.Size(188, 103);
             this.button9.TabIndex = 5;
             this.button9.Text = "Metodos de pago";
             this.button9.UseVisualStyleBackColor = false;
@@ -155,9 +158,9 @@ namespace Cliente
             this.button8.BackColor = System.Drawing.Color.DimGray;
             this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button8.ForeColor = System.Drawing.Color.White;
-            this.button8.Location = new System.Drawing.Point(216, 97);
+            this.button8.Location = new System.Drawing.Point(462, 28);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(141, 62);
+            this.button8.Size = new System.Drawing.Size(207, 103);
             this.button8.TabIndex = 4;
             this.button8.Text = "Tipos de habitacion";
             this.button8.UseVisualStyleBackColor = false;
@@ -168,9 +171,9 @@ namespace Cliente
             this.button4.BackColor = System.Drawing.Color.DimGray;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.ForeColor = System.Drawing.Color.White;
-            this.button4.Location = new System.Drawing.Point(216, 41);
+            this.button4.Location = new System.Drawing.Point(232, 28);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(141, 34);
+            this.button4.Size = new System.Drawing.Size(207, 103);
             this.button4.TabIndex = 3;
             this.button4.Text = "Habitacion";
             this.button4.UseVisualStyleBackColor = false;
@@ -181,9 +184,9 @@ namespace Cliente
             this.button2.BackColor = System.Drawing.Color.DimGray;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(29, 41);
+            this.button2.Location = new System.Drawing.Point(29, 28);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(126, 34);
+            this.button2.Size = new System.Drawing.Size(181, 47);
             this.button2.TabIndex = 2;
             this.button2.Text = "Cliente";
             this.button2.UseVisualStyleBackColor = false;
@@ -194,9 +197,9 @@ namespace Cliente
             this.button3.BackColor = System.Drawing.Color.DimGray;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(29, 97);
+            this.button3.Location = new System.Drawing.Point(29, 81);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(126, 34);
+            this.button3.Size = new System.Drawing.Size(181, 50);
             this.button3.TabIndex = 1;
             this.button3.Text = "Empleado";
             this.button3.UseVisualStyleBackColor = false;
@@ -204,7 +207,7 @@ namespace Cliente
             // 
             // tabPage1
             // 
-            this.tabPage1.BackgroundImage = global::Cliente.Properties.Resources.Captura;
+            this.tabPage1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tabPage1.BackgroundImage")));
             this.tabPage1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.tabPage1.Controls.Add(this.label3);
             this.tabPage1.Controls.Add(this.comboBox2);
@@ -217,7 +220,7 @@ namespace Cliente
             this.tabPage1.Margin = new System.Windows.Forms.Padding(5);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(952, 160);
+            this.tabPage1.Size = new System.Drawing.Size(952, 155);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Conexion sql";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -227,7 +230,8 @@ namespace Cliente
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.label3.Location = new System.Drawing.Point(592, 113);
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Location = new System.Drawing.Point(544, 112);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(39, 36);
             this.label3.TabIndex = 5;
@@ -241,7 +245,7 @@ namespace Cliente
             this.comboBox2.Items.AddRange(new object[] {
             "baseprueba",
             "Hotel Proyecto Moanso"});
-            this.comboBox2.Location = new System.Drawing.Point(284, 76);
+            this.comboBox2.Location = new System.Drawing.Point(285, 63);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(230, 44);
             this.comboBox2.TabIndex = 4;
@@ -250,7 +254,8 @@ namespace Cliente
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(279, 43);
+            this.label2.ForeColor = System.Drawing.Color.Chocolate;
+            this.label2.Location = new System.Drawing.Point(279, 24);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(173, 36);
             this.label2.TabIndex = 3;
@@ -260,7 +265,8 @@ namespace Cliente
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(21, 43);
+            this.label1.ForeColor = System.Drawing.Color.Chocolate;
+            this.label1.Location = new System.Drawing.Point(29, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(135, 36);
             this.label1.TabIndex = 2;
@@ -271,9 +277,9 @@ namespace Cliente
             this.button1.BackColor = System.Drawing.Color.DimGray;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(588, 75);
+            this.button1.Location = new System.Drawing.Point(539, 63);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(125, 35);
+            this.button1.Size = new System.Drawing.Size(163, 46);
             this.button1.TabIndex = 1;
             this.button1.Text = "Ingresar";
             this.button1.UseVisualStyleBackColor = false;
@@ -282,11 +288,12 @@ namespace Cliente
             // comboBox1
             // 
             this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "DESKTOP-NM28KG4\\MSSQLSERVER01",
             "DESKTOP-MSSFJKR\\SQLEXPRESS"});
-            this.comboBox1.Location = new System.Drawing.Point(26, 77);
+            this.comboBox1.Location = new System.Drawing.Point(26, 63);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(230, 44);
             this.comboBox1.TabIndex = 0;
@@ -294,6 +301,7 @@ namespace Cliente
             // 
             // tabControl1
             // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
@@ -301,12 +309,13 @@ namespace Cliente
             this.tabControl1.Location = new System.Drawing.Point(1, 512);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(960, 198);
+            this.tabControl1.Size = new System.Drawing.Size(960, 193);
             this.tabControl1.TabIndex = 0;
             // 
             // Formprincipal
             // 
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(962, 708);
             this.Controls.Add(this.panel1);
--- Cliente/Limpiezadehabitaciones.Designer.cs
+++ Cliente/Limpiezadehabitaciones.Designer.cs
@@ -28,19 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(206, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(225, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(83, 297);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(121, 52);
+            this.treeView1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(206, 164);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tipo de matenimiento";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Limpieza_de_habitaciones
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(936, 540);
+            this.ClientSize = new System.Drawing.Size(1404, 831);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Limpieza_de_habitaciones";
             this.Text = "Limpieza_de_habitaciones";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
     }
 }
