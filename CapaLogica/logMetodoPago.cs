3c70026d3fa2852adc822fc248f7b700998e8a07
yolvin <yolvitoruiz@gmail.com>
2024-06-30 12:38:32 -0500

    Mp delete

--- CapaLogica/logMetodoPago.cs
+++ CapaLogica/logMetodoPago.cs
@@ -10,36 +10,6 @@ namespace CapaLogica
 {
     public class logMetodoPago
     {
-        #region sigleton
-        //Patron Singleton
-        // Variable estática para la instancia.
-        private static readonly logMetodoPago _instancia = new logMetodoPago();
-        //privado para evitar la instanciación directa.
-        public static logMetodoPago Instancia
-        {
-            get
-            {
-                return logMetodoPago._instancia;
-            }
-        }
-        #endregion singleton
-        #region metodos
-        public List<entMetodoPago> ListarMetodoPago()
-        {
-            return datMetodoPago.Instancia.listarMetodoPago();
-        }
-        public void InsertarMetodoPago(entMetodoPago fp)
-        {
-            datMetodoPago.Instancia.InsertarMetodoPago(fp);
-        }
-        public void EditarMetodoPago(entMetodoPago fp)
-        {
-            datMetodoPago.Instancia.EditarMetodoPago(fp);
-        }
-        public void DeshabilitarMetodoPago(int id)
-        {
-            datMetodoPago.Instancia.DeshabilitarMetodoPago(id);
-        }
-        #endregion metodos
+
     }
 }
