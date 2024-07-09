d36b4f34a5b105335665f86d0132560600759b08
James <yrondon@gmail.com>
2024-07-09 18:59:39 -0500

    a

--- CapaDatos/datMetodoPago.cs
+++ CapaDatos/datMetodoPago.cs
@@ -93,7 +93,32 @@ namespace CapaDatos
             return inserta;
         }
 
-        public Boolean Editarmetodopago(entMetodoPago Cli)
+        public Boolean Deshabilitarmetodopago(entMetodoPago Cli)
+        {
+            SqlCommand cmd = null;
+            Boolean delete = false;
+            try
+            {
+                SqlConnection cn = Conexion.Instancia.Conectar();
+                cmd = new SqlCommand("DeshabilitarMetodopago", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", Cli.IDMetodopago);
+                cn.Open();
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    delete = true;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally { cmd.Connection.Close(); }
+            return delete;
+        }
+
+            public Boolean Editarmetodopago(entMetodoPago Cli)
         {
             SqlCommand cmd = null;
             Boolean edita = false;
@@ -120,6 +145,8 @@ namespace CapaDatos
             }
             finally { cmd.Connection.Close(); }
             return edita;
+
+
             #endregion metodos
         }
     }    
