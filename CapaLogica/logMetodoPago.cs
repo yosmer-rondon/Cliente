aaec2139ad8e57f5ee2d038a37f10508155e3e00
ANCELMO <ANCELMO@HP>
2024-07-10 18:52:41 -0500

    y

--- CapaLogica/logMetodoPago.cs
+++ CapaLogica/logMetodoPago.cs
@@ -12,7 +12,7 @@ namespace CapaLogica
     {
         #region sigleton
         //Patron Singleton.
-        // Variable estática para la instancia
+        // Variable estática para la instancia.
         private static readonly logMetodoPago _instancia = new logMetodoPago();
         //privado para evitar la instanciación directa
         public static logMetodoPago Instancia
